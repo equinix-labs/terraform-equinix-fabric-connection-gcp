2c5b4d07a553a79b8ec85815efce9cc9dc91ae63
Oscar Cobles <oscar.cobles@eu.equinix.com>
2022-07-22 13:44:54 +0200

    update fabric connection module versiond and fix typo purchase_order_number was purcharse_order_number

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "google_compute_interconnect_attachment" "this" {
 
 module "equinix-fabric-connection" {
   source = "equinix-labs/fabric-connection/equinix"
-  version = "0.1.1"
+  version = "0.3.1"
 
   # required variables
   notification_users = var.fabric_notification_users
@@ -72,7 +72,7 @@ module "equinix-fabric-connection" {
   service_token_id          = var.fabric_service_token_id
   speed                     = var.fabric_speed
   speed_unit                = "MB"
-  purcharse_order_number    = var.fabric_purcharse_order_number
+  purchase_order_number    = var.fabric_purchase_order_number
 }
 
 module "gcloud-configure-bgp" {
